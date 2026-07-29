0b58172493e7f0739e6f5c06d26689bbe0079582
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-30 00:58:00 +0530

    Time: 32 ms (64.41%), Space: 47.9 MB (55.21%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
@@ -1,19 +1,22 @@
-class Worker implements Comparable<Worker> {
-    int quality;
-    double ratio;
-
-    Worker(int quality, int wage) {
-        this.quality = quality;
-        this.ratio = (double) wage / quality;
-    }
-
-    @Override
-    public int compareTo(Worker other) {
-        return Double.compare(this.ratio, other.ratio);
-    }
-}
-
 class Solution {
+
+    static class Worker implements Comparable<Worker> {
+        int quality;
+        int wage;
+        double ratio;
+
+        Worker(int quality, int wage) {
+            this.quality = quality;
+            this.wage = wage;
+            this.ratio = (double) wage / quality;
+        }
+
+        @Override
+        public int compareTo(Worker other) {
+            return Double.compare(this.ratio, other.ratio);
+        }
+    }
+
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
 
         int n = quality.length;
@@ -22,24 +25,24 @@ class Solution {
         for (int i = 0; i < n; i++) {
             workers[i] = new Worker(quality[i], wage[i]);
         }
-
         Arrays.sort(workers);
+
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        int qualitySum = 0;
+        int totalQuality = 0;
         double ans = Double.MAX_VALUE;
 
         for (Worker worker : workers) {
 
             maxHeap.offer(worker.quality);
-            qualitySum += worker.quality;
+            totalQuality += worker.quality;
 
             if (maxHeap.size() > k) {
-                qualitySum -= maxHeap.poll();
+                totalQuality -= maxHeap.poll();
             }
 
             if (maxHeap.size() == k) {
-                ans = Math.min(ans, qualitySum * worker.ratio);
+                ans = Math.min(ans, totalQuality * worker.ratio);
             }
         }
 
