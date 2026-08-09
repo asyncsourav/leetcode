1abdb79e56f20e197d66646dc7fe37a07b53cf33
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-09 19:58:17 +0530

    Time: 6 ms (77.48%), Space: 47.3 MB (32.5%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-
-        Map<String, PriorityQueue<String>> adj = new HashMap<>();
+        
+        HashMap<String, PriorityQueue<String>> adj = new HashMap<>();
 
         for (List<String> ticket : tickets) {
             String from = ticket.get(0);
@@ -13,15 +13,15 @@ class Solution {
 
         List<String> ans = new ArrayList<>();
         dfs("JFK", adj, ans);
-
+        
         Collections.reverse(ans);
         return ans;
     }
 
-    private void dfs(String airport,
-                     Map<String, PriorityQueue<String>> adj,
-                     List<String> ans) {
-
+    private void dfs(String airport, 
+                    HashMap<String, PriorityQueue<String>> adj,
+                    List<String> ans) {
+        
         PriorityQueue<String> destinations = adj.get(airport);
 
         while (destinations != null && !destinations.isEmpty()) {
