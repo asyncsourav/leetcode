78011a60f1476fd032282c139b682365c5c9b11e
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-21 20:34:56 +0530

    Time: 13 ms (90.2%), Space: 47.8 MB (99.84%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        int[][] dirs = {
+            {1, 0}, {-1, 0},
+            {0, 1}, {0, -1}
+        };
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            
+            int r = curr[0];
+            int c = curr[1];
+
+            for (int[] dir : dirs) {
+
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr >= 0 && nr < m &&
+                    nc >= 0 && nc < n &&
+                    mat[nr][nc] == -1) {
+
+                    mat[nr][nc] = mat[r][c] + 1;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return mat;
+    }
+}
