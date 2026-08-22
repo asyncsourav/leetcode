728f3eb582dcc8d57a02630f57cbac8d7d26ec58
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-22 18:11:37 +0530

    Time: 11 ms (99.31%), Space: 48 MB (12.36%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+
+        int n = grid.length;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+            return -1;
+        }
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0});
+
+        grid[0][0] = 1;
+        int[][] dir = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            { 0, -1},          { 0, 1},
+            { 1, -1}, { 1, 0}, { 1, 1}
+        };
+
+        int pathLength = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+
+                int r = curr[0];
+                int c = curr[1];
+
+                if (r == n - 1 && c == n - 1) {
+                    return pathLength;
+                }
+
+                for (int[] d : dir) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr >= 0 && nr < n &&
+                        nc >= 0 && nc < n &&
+                        grid[nr][nc] == 0) 
+                    {
+                        grid[nr][nc] = 1; 
+                        queue.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+            pathLength++;
+        }
+        return -1;
+    }
+}
