813661e5a9e5076ccc8d432f0d49ef02881cc9fb
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-25 16:58:26 +0530

    Time: 16 ms (20.05%), Space: 74.2 MB (7.77%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,52 +1,47 @@
-
-
-// first check the number of edge is less than the total number of connections
-// then check the number of groups (after connections) - and return the number of groups - 1 
-
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-
-        if (connections.length < n -1)
+        
+        if (connections.length < n - 1) 
             return -1;
 
         List<List<Integer>> adj = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
-        }        
+        }
 
         for (int[] connection : connections) {
             adj.get(connection[0]).add(connection[1]);
             adj.get(connection[1]).add(connection[0]);
         }
 
-        boolean[] visited = new boolean[n];
+        boolean[] visited = new boolean[n + 1];
         int group = 0;
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
+                bfs(adj, i, visited);
                 group++;
-                bfs(adj, visited, i);
             }
         }
 
         return group - 1;
     }
 
-    private void bfs(List<List<Integer>> adj, boolean[] visited, int node) {
+    private void bfs(List<List<Integer>> adj, int node, boolean[] visited) {
         Queue<Integer> queue = new ArrayDeque<>();
 
         visited[node] = true;
         queue.offer(node);
 
         while (!queue.isEmpty()) {
-            int current = queue.poll();
+            int curr = queue.poll();
 
-            for (int neighbor : adj.get(current)) {
-
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.offer(neighbor);
+            for (int x : adj.get(curr)) {
+                
+                if (!visited[x]) {
+                    visited[x] = true;
+                    queue.offer(x);
                 }
             }
         }
